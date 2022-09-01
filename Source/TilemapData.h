25544cd44f331c7c96772a6e64f2b4f55e0689db
balldrix <cballdev@outlook.com>
2022-09-01 23:22:27 +0100

    Add tilemap data struct

---
+++ Source/TilemapData.h
@@ -0,0 +1,54 @@
+#include "pch.h"
+
+#pragma once
+
+namespace 
+{
+	struct TilemapData
+	{
+		unsigned int height = {};
+		std::vector<TilemapLayer> layers;
+		unsigned int tileheight = {};
+		unsigned int tilewidth = {};
+		unsigned int width = {};
+	};
+
+	struct TilemapLayer
+	{
+		std::vector<unsigned int> data;
+		unsigned int height;
+		unsigned int id = {};
+		std::string name = {};
+		unsigned int width = {};
+	};
+
+	void from_json(const json& j, TilemapData& t)
+	{
+		t.height = j.at("height").get<unsigned int>();
+		json layers = j["layers"];		
+
+		for(auto i = layers.begin(); i != layers.end(); ++i)
+		{
+			json layer = i.value();
+			json data = layer["data"];
+
+			TilemapLayer tilemapLayer;
+
+			for(auto j = data.begin(); j != data.end(); ++j)
+			{
+				tilemapLayer.data.push_back(j.value());
+			}
+
+			tilemapLayer.height = layer["height"];
+			tilemapLayer.id = layer["id"];
+			tilemapLayer.name = layer["name"];
+			tilemapLayer.width = layer["width"];
+
+			t.layers.push_back(tilemapLayer);
+		}
+		
+		t.tileheight = j.at("tileheight").get<unsigned int>();
+		t.tilewidth = j.at("tilewidth").get<unsigned int>();
+		t.width = j.at("width").get<unsigned int>();
+	}
+}
