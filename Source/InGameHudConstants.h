0ccfe612a549c803f29417bfb3d9e59f06e6b54d
balldrix <cballdev@outlook.com>
2024-10-01 19:27:44 +0100

    move kill count

--- Source/InGameHudConstants.h
+++ Source/InGameHudConstants.h
@@ -11,7 +11,7 @@ namespace InGameHudConstants
 	const unsigned int LifeCounterPositionY = 6;
 	const unsigned int TravelPromptPositionX = 88;
 	const unsigned int TravelPromptPositionY = 16;
-	const unsigned int KillCountPositionX = 115;
+	const unsigned int KillCountPositionX = 220;
 	const unsigned int KillCountPositionY = 10;
 	const float TravelPromptTime = 3.0f;
 }
